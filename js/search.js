ad6260cf2a15458d4c3fc58b1cff7d652202b444
Zac Smith <zrs3141592@gmail.com>
2019-05-16 23:59:49 -0600

    github user input

--- js/search.js
+++ js/search.js
@@ -1,4 +1,6 @@
-function githubUser() {
-    const userName = document.querySelector('#userSearch').value.toLowerCase();
-    console.log(userName);
-}
+    function githubUserInput() {
+        const userName = document.querySelector('#userSearch').value.toLowerCase();
+        console.log(userName);
+    }
+
+// REGEX (/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i)
