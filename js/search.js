cb75abadc49a998bddc8b08a1f959f2a3874e171
Zac Smith <zrs3141592@gmail.com>
2019-05-19 14:17:56 -0600

    reduced footer margin/padding to 0.75

--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
-    function githubUserInput() {
-        const userName = document.querySelector('#userSearch').value.toLowerCase();
-        console.log(userName);
-    }
+function githubUserInput() {
+    const userName = document.querySelector('#userSearch').value.toLowerCase();
+    console.log(userName);
+}
 
 // REGEX (/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i)
+
